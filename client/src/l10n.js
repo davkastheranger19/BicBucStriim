1d6c3b643115609e7161f3af86b865cb43721869
Rainer Volz <dev@textmulch.de>
2017-03-08 15:50:16 +0100

    Migrated localizations

--- client/src/l10n.js
+++ client/src/l10n.js
@@ -1,22 +1,24 @@
 import LocalizedStrings from 'react-localization'
 
+// TODO load language files dynamically, ca. 20KB!
+import msgs_en from './data/l10n_en.json';
+import msgs_de from './data/l10n_de.json';
+import msgs_es from './data/l10n_es.json';
+import msgs_fr from './data/l10n_fr.json';
+import msgs_gl from './data/l10n_gl.json';
+import msgs_hu from './data/l10n_hu.json';
+import msgs_it from './data/l10n_it.json';
+import msgs_nl from './data/l10n_nl.json';
+
 export function Locs() { 
 	return new LocalizedStrings({
-	      en:{
-	        home:"Home",
-	        titles:"Titles",
-	        authors:"Authors",
-	        tags:"Tags",
-	        series:"Series",
-	        most_recent: "Most recent"
-	      },
-	      de: {
-	        home:"Start",
-	        titles:"Bücher",
-	        authors:"Autoren",
-	        tags:"Schlagworte",
-	        series:"Serien",
-	        most_recent: "Die neuesten"
-	      }
-	    })
+	      en: msgs_en,
+	      de: msgs_de,
+	      es: msgs_es,
+	      fr: msgs_fr,
+	      gl: msgs_fr,
+	      hu: msgs_hu,
+	      it: msgs_it,
+	      nl: msgs_nl
+	})		
 }
