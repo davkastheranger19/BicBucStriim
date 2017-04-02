33ca1b292d3fd98643b84d2d618d705c0f86e8bb
Rainer Volz <rainer.volz@storema.de>
2017-04-02 19:43:13 +0200

    Added OPDS unit tests

--- api/src/dependencies.php
+++ api/src/dependencies.php
@@ -1,6 +1,7 @@
 <?php
 require_once 'BicBucStriim/bicbucstriim.php';
 require_once 'BicBucStriim/calibre.php';
+require_once 'BicBucStriim/calibre_filter.php';
 require_once 'BicBucStriim/app_constants.php';
 require_once 'BicBucStriim/mailer.php';
 require_once 'BicBucStriim/token.php';
